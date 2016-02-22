d6936b45a61d60a2496f40629533608ae1d5c656
Frédérand Ouweric <pacm@users.noreply.github.com>
2016-02-22 10:53:17 +0100

    init slf4j (#7)

--- projects/coursera-scrapper/src/main/java/Scrapper.java
+++ projects/coursera-scrapper/src/main/java/Scrapper.java
@@ -1,9 +1,13 @@
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 public class Scrapper {
+    private static final Logger logger = LoggerFactory.getLogger(Scrapper.class);
+
     public static void main(String[] args) {
         WebClient webClient = new WebClient();
         final HtmlPage page;
@@ -11,7 +15,7 @@ public class Scrapper {
             page = webClient.getPage("http://htmlunit.sourceforge.net");
             String pageAsText = page.asText();
 
-            System.out.println(pageAsText);
+            logger.info("Fist letters of the page: " + pageAsText.substring(0, 10));
 
         } catch (IOException e) {
             e.printStackTrace();
