df03398e3daeec8eacd84569b99f6d5e988f7780
Frédérand Ouweric <pacm@users.noreply.github.com>
2016-02-22 10:49:54 +0100

    init html-unit (#7)

--- projects/coursera-scrapper/src/main/java/Scrapper.java
+++ projects/coursera-scrapper/src/main/java/Scrapper.java
@@ -1,6 +1,20 @@
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+import java.io.IOException;
 
 public class Scrapper {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        WebClient webClient = new WebClient();
+        final HtmlPage page;
+        try {
+            page = webClient.getPage("http://htmlunit.sourceforge.net");
+            String pageAsText = page.asText();
+
+            System.out.println(pageAsText);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
