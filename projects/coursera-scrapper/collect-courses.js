5d686188a82a46dfa9d50afa80fed50725c9cfe1
Frédérand Ouweric <pacm@users.noreply.github.com>
2016-02-23 23:41:14 +0100

    resumable script to collect courses (#7)

---
+++ projects/coursera-scrapper/collect-courses.js
@@ -0,0 +1,175 @@
+var fs = require('fs');
+var wp = require('webpage');
+
+// Includes
+// Generic wait-for function
+function waitFor(testFn, successFn, timeOutFn, finallyFn, timeOutMs, refreshMs) {
+    var start = new Date().getTime(),
+        condition = false,
+        interval = setInterval(function() {
+    	var elapsed = new Date().getTime() - start;
+        if((elapsed < timeOutMs) && !condition) {
+            condition = testFn(elapsed);
+        } else {
+            if(!condition) {
+                timeOutFn(elapsed);
+                finallyFn();
+            } else {
+                successFn();
+                finallyFn();
+                clearInterval(interval);
+            }
+        }
+    }, refreshMs);
+};
+
+// ReplaceAll
+// source: http://stackoverflow.com/questions/1144783/replacing-all-occurrences-of-a-string-in-javascript
+function replaceAll(str, search, replacement) {
+    return str.replace(new RegExp(search, 'g'), replacement);
+};
+
+// Handling in/out put
+var courseLinksPath = 'output/course-links.txt';
+var data = JSON.parse(fs.read(courseLinksPath));
+var outputPath = 'output/courses';
+
+// Collecting Job
+// ..
+// Create the page
+var page = wp.create();
+page.settings.userAgent = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:44.0) Gecko/20100101 Firefox/44.0";
+var width = 1280;
+var height = 1024;
+page.viewportSize = {
+  width: width,
+  height: height
+};
+/* Uncomment for log messages in .evaluate sections
+page.onConsoleMessage = function(msg) {
+  console.log(msg);
+};
+// */
+
+// Open each domain link
+var timeoutMs = 60 * 60 * 1000;
+var refreshMs = 1 * 1000;
+var courseraDomain = 'https://www.coursera.org';
+var pathPrefix = 'output/courses';
+var paths = {
+	pages: pathPrefix + '/pages',
+	screenshot: pathPrefix + '/screenshots',
+};
+
+function closeAndExit(page) {
+	// Finally, quitting
+	console.log('Quitting Script');
+	page.close();
+	phantom.exit();
+}
+function openLink(data, domainPosition, subdomainPosition, coursePosition) {
+	// define go next function
+	function goNext() {
+        if((coursePosition + 1) < list.courses.length) {
+        	openLink(data, domainPosition, subdomainPosition, coursePosition + 1);
+			goToNextDomain = false;
+        } else {
+            // Check if we are in a subdomain
+            if(typeof subdomain != "undefined") {
+    			// Check if there are other subdomains
+    			if((subdomainPosition + 1) < domain.subdomain.length) {
+    				openLink(data, domainPosition, subdomainPosition + 1, 0);
+    				goToNextDomain = false;
+    			}
+            }
+        }
+
+        if(goToNextDomain) {
+        	// Check if there is a next domain
+        	if((domainPosition + 1) < data.length) {
+        		openLink(data, domainPosition + 1, 0, 0);
+        	} else {
+	            // Quit
+            	closeAndExit(page);
+        	}
+        }
+	}
+
+	// Get current list
+	var domain = data[domainPosition];
+	if('subdomain' in domain) {
+		var subdomain = data[domainPosition].subdomain[subdomainPosition];
+		var list = subdomain;
+		var uniqueName = domainPosition + '-' + subdomainPosition;
+	} else {
+		subdomain = undefined;
+		list = domain;
+		uniqueName = domainPosition;
+	}
+	var course = list.courses[coursePosition];
+	uniqueName = uniqueName + '-' + coursePosition;
+	if(course.hasOwnProperty('localPath') && course.hasOwnProperty('screenshot')) {
+		console.log('Passing course ' + uniqueName);
+		goNext();
+	} else {
+		page.open(courseraDomain + course.href, function(status) {
+			// Check for page load success
+			if(status !== 'success') {
+				console.log("Unable to access network, status: " + status);
+			} else {
+				var aboutSel = '.about-container';
+				var aboutSel2 = '.c-cd-section';
+
+		        // Wait for page-load
+		        waitFor(
+		        	function(elapsed) {
+		        		// Test if the page has loaded
+			            return page.evaluate(function(aboutSel, aboutSel2) {
+			            	var about = $(aboutSel);
+			            	var about2 = $(aboutSel2);
+			            	return (about.text().length > 0) || (about2.text().length > 0);
+			            }, aboutSel, aboutSel2);
+			        }, function(elapsed) {
+			        	// When we think that the page has loaded
+			        	// ..
+			        	// wait an additional time
+			        	setTimeout(function() {
+				        	// Download the course
+							var content = page.content;
+							var localPath = paths.pages + '/' + uniqueName + '.html';
+							fs.write(localPath, content, 'w');
+							console.log('Course ' + localPath + ' written');
+
+							// Take a screenshot
+							var screenshotPath = paths.screenshot + '/' + uniqueName + '.png';
+							page.render(screenshotPath, {
+				            	format: 'png',
+				            	quality: '100'
+					        });
+
+							// Save the path to the course file
+							course.localPath = localPath;
+							course.screenshot = screenshotPath;
+
+				            // Write course current info
+				            fs.write(courseLinksPath, JSON.stringify(data, null, 3), 'w');
+
+				            // Continue the work
+				            goNext();
+			        	}, 3 * 1000);
+			        }, function(elapsed) {
+			        	// If the page cannot be loaded
+			        	console.error("Cannot load the course: " + 
+			        		course.label + "(" + course.href + ")");
+			        	page.render(paths.screenshot + "/" + "error.png");
+			        	closeAndExit(page);
+			        }, function() {
+			        	// nothing here
+					}, timeoutMs, refreshMs
+			    );
+			}
+		});
+	}
+}
+
+openLink(data, 0, 0, 0);
