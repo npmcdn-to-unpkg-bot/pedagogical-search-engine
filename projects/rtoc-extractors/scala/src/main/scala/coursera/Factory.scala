237b511430444a1cedae31f470c5ae998ff59269
Frédérand Ouweric <pacm@users.noreply.github.com>
2016-03-04 17:10:27 +0100

    refactor (#13)

--- projects/rtoc-extractors/scala/src/main/scala/coursera/Factory.scala
+++ projects/rtoc-extractors/scala/src/main/scala/coursera/Factory.scala
@@ -4,7 +4,8 @@ import java.io.File
 
 import Utils.Logger
 import coursera.Types.Course
-import coursera.layouts.Simple
+import coursera.layouts.{Inline, Simple}
+import org.json4s.JsonAST.JValue
 import org.json4s.JsonDSL._
 import org.jsoup.Jsoup
 import rtoc.{Node, Resource}
@@ -16,68 +17,23 @@ class Factory(pages: File, outputFolder: File) extends rtoc.Factory[Course](outp
   val baseURL = "http://www.coursera.com/"
 
   override def produceResources(course: Course): List[Resource] = {
-    Logger.info("Processing " + course.toString)
-
     // Parse
     val doc = parse(course)
 
     // Extract the informations
     try {
       // Match the page layout
-      val p = doc match {
-        case Simple(p) => Some(p)
-        case _ => None
-      }
-
-      p match {
-        // Unknown layout
-        case None => Nil
-        // Layout was recognized
-        case Some(pair) => {
-
-          // Create the metadata
-          val partners = course.partner match {
-            case Some(partner) => partner::Nil
-            case None => Nil
-          }
-          val metadata1 =
-            ("source" -> "coursera") ~ ("level" -> "university") ~
-              ("title" -> course.label) ~ ("href" -> course.href) ~
-              ("partners" -> partners) ~ ("miniature" -> course.localImg) ~
-              ("screenshot" -> course.screenshot) ~ ("domain" -> course.domain) ~
-              ("subdomain" -> course.subdomain)
-
-          // Merge metadata
-          val metadata = pair._2 match {
-            case None => metadata1
-            case Some(metadata2) => metadata2.merge(metadata1)
-          }
-
-          // Extend toc with domain, subdomain
-          def addDomain(nodes: List[Node]): List[Node] = course.domain match {
-            case Some(domain) => new Node(domain, nodes)::Nil
-            case None => nodes
-          }
-
-          def addSubdomain(nodes: List[Node]): List[Node] = course.subdomain match {
-            case Some(subdomain) => new Node(subdomain, nodes)::Nil
-            case None => nodes
-          }
-
-          def completeNodes(nodes: List[Node]): List[Node] = addDomain(addSubdomain(nodes))
-
-          val toc = completeNodes(pair._1)
-
-          // Create the resource
-          val outPath = outputFolder.getAbsolutePath
-          val resource = new Resource(
-            toc,
-            metadata,
-            s"$outPath/coursera",
-            name(course.href)
-          )
-          resource::Nil
+      val href = course.href
+      doc match {
+        case Simple(p) => {
+          Logger.info(s"Simple-layout: $href")
+          produceResource(course, p)
         }
+        case Inline(p) => {
+          Logger.info(s"Inline-layout: $href")
+          produceResource(course, p)
+        }
+        case _ => Nil
       }
     } catch {
       // No resource was created
@@ -90,8 +46,53 @@ class Factory(pages: File, outputFolder: File) extends rtoc.Factory[Course](outp
     }
   }
 
+  def produceResource(course: Course, pair: (List[Node], Option[JValue])): List[Resource] = {
+    // Create the metadata
+    val partners = course.partner match {
+      case Some(partner) => partner::Nil
+      case None => Nil
+    }
+    val metadata1 =
+      ("source" -> "coursera") ~ ("level" -> "university") ~
+        ("title" -> course.label) ~ ("href" -> course.href) ~
+        ("partners" -> partners) ~ ("miniature" -> course.localImg) ~
+        ("screenshot" -> course.screenshot) ~ ("domain" -> course.domain) ~
+        ("subdomain" -> course.subdomain)
+
+    // Merge metadata
+    val metadata = pair._2 match {
+      case None => metadata1
+      case Some(metadata2) => metadata2.merge(metadata1)
+    }
+
+    // Extend toc with domain, subdomain
+    def addDomain(nodes: List[Node]): List[Node] = course.domain match {
+      case Some(domain) => new Node(domain, nodes)::Nil
+      case None => nodes
+    }
+
+    def addSubdomain(nodes: List[Node]): List[Node] = course.subdomain match {
+      case Some(subdomain) => new Node(subdomain, nodes)::Nil
+      case None => nodes
+    }
+
+    def completeNodes(nodes: List[Node]): List[Node] = addDomain(addSubdomain(nodes))
+
+    val toc = completeNodes(pair._1)
+
+    // Create the resource
+    val outPath = outputFolder.getAbsolutePath
+    val resource = new Resource(
+      toc,
+      metadata,
+      s"$outPath/coursera",
+      name(course.href)
+    )
+    resource::Nil
+  }
+
   def name(href: String): String =
-    MurmurHash3.stringHash(s"scholarpedia$href").toString
+    MurmurHash3.stringHash(s"coursera$href").toString
 
   def parse(course: Course) = {
     val name = course.localPath.split("/").toList.reverse.head
--- projects/rtoc-extractors/scala/src/main/scala/coursera/layouts/Simple.scala
+++ projects/rtoc-extractors/scala/src/main/scala/coursera/layouts/Simple.scala
@@ -1,48 +1,36 @@
 package coursera.layouts
 
-import Utils.Logger
 import org.json4s.JsonAST.JValue
-import org.jsoup.nodes.{Document, Element}
-import org.jsoup.select.Elements
-import rtoc.Node
+import org.jsoup.nodes.Document
+import rtoc.{LayoutExtractor, Node}
 
-import scala.collection.JavaConverters._
-
-object Simple {
-  def unapply(doc: Document): Option[(List[Node], Option[JValue])] = {
+object Simple extends LayoutExtractor[(List[Node], Option[JValue])] {
+  def unapply(doc: Document) = {
     try {
-      // Course label
-      val label = l(doc.select("h1.course-name")) match {
-        case e::Nil => e.text()
+      // For each Weekly-module
+      val nodes = l(doc.select("div.rc-Syllabus > div.week-entry")) match {
+        case weekEntries @ (xs::x) => weekEntries.map(weekEntry => {
+          // module title
+          val title = l(weekEntry.select("p.module-name")) match {
+            case module::Nil => module.text()
+          }
+
+          // module topics
+          val topics = l(weekEntry.select("ol.week-topics > li")) match {
+            case t @ x::xs => t.map(_.text())
+          }
+
+          // Create node
+          new Node(title, topics.map(new Node(_, Nil)))
+        })
       }
 
-      // For each Weekly-module
-      val nodes = l(doc.select("div.rc-Syllabus > div.week-entry")).map(weekEntry => {
-        // module title
-        val title = l(weekEntry.select("p.module-name")) match {
-          case module::Nil => module.text()
-        }
-
-        // module topics
-        val topics = l(weekEntry.select("ol.week-topics > li")) match {
-          case t @ x::xs => t.map(_.text())
-        }
-
-        // Create node
-        new Node(title, topics.map(new Node(_, Nil)))
-      })
-
-      // Create metadata
-      val metadata: Option[JValue] = None
-
       // Create the extracted pairs
-      val pairs = (nodes, metadata)
+      val pairs = (nodes, None)
 
       Some(pairs)
     } catch {
       case e => None
     }
   }
-
-  def l(es: Elements): List[Element] = es.iterator().asScala.toList
 }
