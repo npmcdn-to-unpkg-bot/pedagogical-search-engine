b77c4a70c8acecde15e26f63928e3a7950619d76
Frédérand Ouweric <pacm@users.noreply.github.com>
2016-03-09 14:16:32 +0100

    add metadata information (#1)

--- projects/rtoc-extractors/scala/src/main/scala/safaribooks/Factory.scala
+++ projects/rtoc-extractors/scala/src/main/scala/safaribooks/Factory.scala
@@ -2,15 +2,21 @@ package safaribooks
 
 import java.io.File
 
-import org.jsoup.nodes.Document
+import org.json4s.JsonAST.JString
 import rsc.Resource
 import safaribooks.Types.Book
-import utils.Conversions.hash
+import utils.Conversions.{hash, normalize}
+import utils.Logger
 
 class Factory(outputFolder: File) extends rsc.Factory[Book] {
   override def getOrFail(book: Book): Resource = {
     // Open page
     val doc = open(book.path)
+
+    // Metadata
+    val metaEl = doc match {
+      case layouts.metadata.Simple(re) => re
+    }
 
     // TOC
     val tocEl = doc match {
@@ -19,7 +25,14 @@ class Factory(outputFolder: File) extends rsc.Factory[Book] {
 
     // Create ressource
     val folderPath = outputFolder.getAbsolutePath + "/safari"
-    val name = hash(book.path)
-    tocEl.resource(folderPath, name)
+    val title = metaEl.oMetadata match {
+      case Some(m) => m \\ "title" match {
+        case JString(title) => normalize(title)
+      }
+    }
+    val name = title + "-" + hash(book.path)
+    val mergedEl = metaEl.mergeWith(tocEl)
+    Logger.info(s"$name: OK")
+    mergedEl.resource(folderPath, name)
   }
 }
