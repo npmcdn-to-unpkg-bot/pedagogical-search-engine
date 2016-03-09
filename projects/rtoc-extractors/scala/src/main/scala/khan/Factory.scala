6834ddd4f9424b5d19c530988146a4019e813d89
Frédérand Ouweric <pacm@users.noreply.github.com>
2016-03-09 09:21:16 +0100

    extract toc (#17)

--- projects/rtoc-extractors/scala/src/main/scala/khan/Factory.scala
+++ projects/rtoc-extractors/scala/src/main/scala/khan/Factory.scala
@@ -3,7 +3,6 @@ package khan
 import java.io.File
 
 import khan.Types.Course
-import khan.layouts.metadata.Normal
 import org.json4s.JsonAST.JString
 import org.json4s.JsonDSL._
 import org.jsoup.Jsoup
@@ -20,16 +19,19 @@ class Factory(outputFolder: File) extends rsc.Factory[Course] {
     // Open course
     val doc = open(course.path)
 
+    // TOC
+    val tocEl = doc match {
+      case layouts.toc.Normal(re) => re
+    }
+
     // Metadata
     val extractedEl = doc match {
-      case Normal(re) => re
+      case layouts.metadata.Normal(re) => re
     }
     val additionalEl = ResourceElement(Some(
       ("source" -> "khanacademy") ~ ("level" -> "fundamental") ~
       ("categories" -> course.parents)
     ), None, None)
-
-    val metaEl = extractedEl.mergeWith(additionalEl)
 
     // Create name
     val title = extractedEl.oMetadata match {
@@ -40,14 +42,10 @@ class Factory(outputFolder: File) extends rsc.Factory[Course] {
     val name = (course.parents:::List(title)).map(normalize(_)).mkString("-")
 
     // Create resource
-    val outputPath = outputFolder.getAbsolutePath
     Logger.info(s"$name: OK")
-    new Resource(
-      metaEl.oMetadata,
-      None,
-      None,
-      s"$outputPath/khan",
-      name)
+    val mergedEl = tocEl.mergeWith(extractedEl, additionalEl)
+    val outputPath = outputFolder.getAbsolutePath + "/khan"
+    mergedEl.resource(outputPath, name)
   }
 
   def open(path: String): Document = Jsoup.parse(new File(path), utf8, baseURL)
