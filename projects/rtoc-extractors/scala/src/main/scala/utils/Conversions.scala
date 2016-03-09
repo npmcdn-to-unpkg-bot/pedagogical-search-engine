89ed4b031e10223e6b9bb9402b3733e07befc6c2
Frédérand Ouweric <pacm@users.noreply.github.com>
2016-03-09 14:14:50 +0100

    Conversions: fix hash, generalize hash and text

--- projects/rtoc-extractors/scala/src/main/scala/utils/Conversions.scala
+++ projects/rtoc-extractors/scala/src/main/scala/utils/Conversions.scala
@@ -7,18 +7,18 @@ import scala.collection.JavaConverters._
 import scala.util.hashing.MurmurHash3
 
 object Conversions {
-  def l[U](javaList: java.util.List[U]): List[U] = javaList.iterator().asScala.toList
+  def l[U](javaList: java.lang.Iterable[U]): List[U] = javaList.iterator().asScala.toList
 
   def toBuffer[U](a: List[U]): mutable.Buffer[U] = mutable.Buffer().++(a)
 
   def normalize(s: String): String = s.replaceAll("[^a-zA-Z0-9 ]", "").toLowerCase.trim
 
-  def textualize(s: String): String = s.replaceAll("\\u00a0", " ")
+  def textualize(s: String): String = s.replaceAll("\\u00a0", " ").replaceAll("\\u00ae", "")
 
   def text(e: Element): String = textualize(e.text())
 
   def hash(s: String): String = MurmurHash3.stringHash(s) match {
-    case n if n < 0 => "0" + n
+    case n if n < 0 => "0" + Math.abs(n)
     case p => "1" + p
   }
 }
