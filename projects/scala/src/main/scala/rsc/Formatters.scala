4ebd0d2bbe7d683796bb4477316130f8492facc1
Frédérand Ouweric <pacm@users.noreply.github.com>
2016-03-15 15:55:37 +0100

    fix json spots-extraction with Spotlight-Formatter

--- projects/scala/src/main/scala/rsc/Formatters.scala
+++ projects/scala/src/main/scala/rsc/Formatters.scala
@@ -1,14 +1,17 @@
 package rsc
 
-import org.json4s.DefaultFormats
 import org.json4s.ext.EnumNameSerializer
+import org.json4s.{DefaultFormats, ShortTypeHints}
 import rsc.annotators.Annotator
+import rsc.attributes.Candidate.Spotlight
 import rsc.attributes.{Level, Source}
+import rsc.indexers.Indexer
 
 trait Formatters {
   implicit val formats = DefaultFormats ++ Seq(
     new EnumNameSerializer(Source),
     new EnumNameSerializer(Level),
-    new EnumNameSerializer(Annotator)
-  )
+    new EnumNameSerializer(Annotator),
+    new EnumNameSerializer(Indexer)
+  ) + ShortTypeHints(List(classOf[Spotlight]))
 }
