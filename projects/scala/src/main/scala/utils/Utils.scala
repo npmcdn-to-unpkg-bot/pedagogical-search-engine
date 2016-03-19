c08ae42557537b46c09be2200ce2621b1971b36c
Frédérand Ouweric <pacm@users.noreply.github.com>
2016-03-19 22:31:56 +0100

    implement merge-options into lists

--- projects/scala/src/main/scala/utils/Utils.scala
+++ projects/scala/src/main/scala/utils/Utils.scala
@@ -6,4 +6,14 @@ object Utils {
     case (x, None) => x
     case (Some(v1), Some(v2)) => Some(f(v1, v2))
   }
+  def mergeOptions2List[A](os: Option[A]*): List[A] = {
+    def mergeRec(acc: List[A], os: Option[A]*): List[A] = os.toList match {
+      case Nil => acc
+      case head::tail => head match {
+        case None => mergeRec(acc, tail: _*)
+        case Some(e) => mergeRec(acc:::List(e), tail: _*)
+      }
+    }
+    mergeRec(List(), os: _*)
+  }
 }
