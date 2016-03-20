1d5932430fb5e5686115ef68532b366036ea5fcb
Frédérand Ouweric <pacm@users.noreply.github.com>
2016-03-20 22:38:56 +0100

    annotate toc-nodes (#29)

--- projects/scala/src/main/scala/rsc/indexers/Graph.scala
+++ projects/scala/src/main/scala/rsc/indexers/Graph.scala
@@ -1,17 +1,101 @@
 package rsc.indexers
 
 import graph.edges.unbiased.AttachedWeight
-import graph.{Pagerank, Utils}
+import graph.{DirectedGraph, Pagerank, Utils}
 import mysql.GraphFactory
 import rsc.Resource
+import rsc.Types.{Nodes, Indices}
 import rsc.attributes.Candidate.{Candidate, Spotlight}
 import utils.Constants
 import utils.Utils.mergeOptions2List
+import utils.Math._
 
 import scala.collection.JavaConverters._
 
 class Graph {
   def index(r: Resource): Option[Resource] = {
+    constructGraph(r).flatMap { case (digraph, allUris) => {
+      // Index Title
+      index(digraph, allUris) match {
+        case Nil => None
+        case titleIndices => {
+          // Index the tocs
+          val newOTocs = r.oTocs.map(_.map(toc => {
+            val newNodes = indexNodes(toc.nodes)(digraph)
+            toc.copy(nodes = newNodes)
+          }))
+
+          // Create the new resource
+          Some(
+            r.copy(
+              title = r.title.copy(oIndices = Some(titleIndices)),
+              oTocs = newOTocs
+              //oIndexer = Some(Indexer.Graph)
+            )
+          )
+        }
+      }
+    }}
+  }
+
+  private def indexNodes(nodes: Nodes)(implicit digraph: DirectedGraph)
+  : Nodes = nodes match {
+    case Nil => Nil
+    case _ => nodes.map(node => {
+      // Index children
+      val newChildren = indexNodes(node.children)
+
+      // Index the current Node
+      val subNodes = node::node.childrenRec()
+      val uris = skim(
+        mergeOptions2List(subNodes.map(_.oSpots): _*)
+          .flatten
+          .flatMap(_.candidates)
+      ).map(_.uri).toSet
+
+      // Using pagerank
+      println(s"node $node")
+      val nodeIndices = index(digraph, uris)
+
+      // Create the new node
+      node.copy(
+        children = newChildren,
+        oIndices = Some(nodeIndices)
+      )
+    })
+  }
+
+  private def index(digraph: DirectedGraph, uris: Set[String])
+  : Indices = uris.size match {
+    case zero if zero == 0 => Nil
+    case _ => {
+      // Run the pagerank
+      val nWeight = new graph.nodes.biased.Uniform(digraph, uris.asJava)
+      val eWeight = new AttachedWeight(
+        digraph,
+        Constants.Graph.Edges.Attribute.completeWlm,
+        Constants.Graph.Edges.Attribute.normalizedCwlm)
+
+      Pagerank.weighted(digraph, nWeight, eWeight, 0.8)
+
+      // Produce the indices
+      val toKeep = math.ceil(uris.size.toDouble * 0.8).toInt
+      val topNodes = digraph.getNodes.asScala.toList
+        .sortBy(-_.getScore).take(toKeep)
+      println(s"taking $toKeep, uris: {$uris}")
+      topNodes.map(println(_))
+      val topScores = rescaleD(
+        normalizeD(
+          topNodes.map(_.getScore().toDouble)
+        )
+      )
+      val topURIs = topNodes.map(_.getId())
+
+      topURIs.zip(topScores).map(p => Index(p._1, p._2))
+    }
+  }
+
+  def constructGraph(r: Resource): Option[(DirectedGraph, Set[String])] = {
     // Collect the candidate senses
     val candidates = skim(mergeOptions2List(
       r.title.oSpots,
@@ -28,13 +112,16 @@ class Graph {
 
     ).flatten.flatMap(_.candidates))
 
-    // Build the digraph
+    // Build a first digraph
     val uris = candidates.map(_.uri.toLowerCase)
     val digraph = GraphFactory.connect(uris.asJava)
 
-    // Index the graph
+    // Build a second digraph
     digraph.getNodes.asScala.toList match {
-      case Nil => None // fail if there are no nodes
+      case Nil => {
+        println(s"no nodes: ${uris.size}")
+        None
+      } // fail if there are no nodes
       case nodes => {
         // Get the principal connected component
         val ccs = Utils.connectedComponents(nodes)
@@ -42,31 +129,10 @@ class Graph {
 
         // Build a better graph
         val uris2 = biggestCC.map(_.getId)
-        val digraph2 = GraphFactory.smart2(uris2.asJava)
-
-        // Run the pagerank
-        val nWeight = new graph.nodes.biased.Uniform(digraph2, uris2.asJava)
-        val eWeight = new AttachedWeight(
-          digraph2,
-          Constants.Graph.Edges.Attribute.completeWlm,
-          Constants.Graph.Edges.Attribute.normalizedCwlm)
-
-        Pagerank.weighted(digraph2, nWeight, eWeight, 0.8)
-
-        // Add the indices to the resource
-        val toKeep = math.ceil(biggestCC.size.toDouble * 0.8).toInt
-        val topNodes = digraph2.getNodes.asScala.toList
-          .sortBy(-_.getScore).take(toKeep)
-
-        topNodes.map(node => Index(node.getId(), node.getScoreOrZero)) match {
-          case Nil => None
-          case indexes => Some(
-            r.copy(
-              title = r.title.copy(oIndices = Some(indexes)),
-              oIndexer = Some(Indexer.Graph)
-            )
-          )
-        }
+        Some(
+          GraphFactory.smart2(uris2.asJava),
+          uris2
+        )
       }
     }
   }
