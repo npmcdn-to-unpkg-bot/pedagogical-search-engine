ac1ac5b9d9c7e5a0ba82284e97b92d444e2e6a45
Frédérand Ouweric <pacm@users.noreply.github.com>
2016-03-24 23:27:10 +0100

    batch annotation (#32)

--- projects/scala/src/main/scala/rsc/annotators/Standard.scala
+++ projects/scala/src/main/scala/rsc/annotators/Standard.scala
@@ -15,81 +15,128 @@ object Standard {
 
   private def newResource(r: Resource, ws: WebService): Resource = {
 
-    // Function to annotate the nodes recursively
-    def annotateNodes(nodes: Nodes): Nodes = nodes match {
-      case Nil => Nil
-      case _ => {
-        val labels = nodes.map(_.label)
-        ws.annotateSeparately(labels) match {
-          case Some(l) => l.zip(nodes).map {
-            case (spots, node) => {
-              val newChildren = annotateNodes(node.children)
-              node.copy(oSpots = Some(spots), children = newChildren)
-            }
-          }
-        }
+    //
+    val titleWork: List[(Object, String)] = (r.title -> r.title.label)::Nil
+
+    //
+    val keyWork: List[(Object, String)] = r.oKeywords match {
+      case None => Nil
+      case Some(keywords) => keywords.map {
+        case keyword => (keyword -> keyword.label)
       }
     }
+
+    //
+    val catWork: List[(Object, String)] = r.oCategories match {
+      case None => Nil
+      case Some(categories) => categories.map {
+        case category => (category -> category.label)
+      }
+    }
+
+    //
+    val domWork: List[(Object, String)] = r.oDomains match {
+      case None => Nil
+      case Some(domains) => domains.map {
+        case domain => (domain -> domain.label)
+      }
+    }
+
+    //
+    val subdomWork: List[(Object, String)] = r.oSubdomains match {
+      case None => Nil
+      case Some(subdomains) => subdomains.map {
+        case subdomain => (subdomain -> subdomain.label)
+      }
+    }
+
+    //
+    val desWork: List[(Object, String)] = r.oDescriptions match {
+      case None => Nil
+      case Some(descriptions) => descriptions.map {
+        case description => (description -> description.text)
+      }
+    }
+
+    //
+    def getNodesWork(nodes: Nodes): List[(Object, String)] =
+      nodes.map(node => {
+        val labelWork = (node -> node.label)
+        labelWork::getNodesWork(node.children)
+      }) match {
+        case Nil => Nil
+        case xs => xs reduce (_ ::: _)
+      }
+
+    val nodeWork: List[(Object, String)] = r.oTocs match {
+      case None => Nil
+      case Some(tocs) => tocs.map(toc => {
+        getNodesWork(toc.nodes)
+      }) match {
+        case Nil => Nil
+        case xs => xs reduce (_ ::: _)
+      }
+    }
+
+    //
+    val work: List[(Object, String)] =
+      titleWork ::: keyWork ::: catWork ::: domWork ::: subdomWork ::: desWork ::: nodeWork
+
+    //
+    val results: Map[Object, Spots] = ws.annotateTogether(
+      work.map {
+        case (obj, text) => text
+      }
+    ) match {
+      case Some(spotsList) => work.map {
+        case (obj, _) => obj
+      }.zip(spotsList).toMap
+    }
+
+    // Function to annotate the nodes recursively
+    def annotateNodes(nodes: Nodes): Nodes = nodes.map(node => {
+      val spots = results(node)
+      node.copy(oSpots = Some(spots), children = annotateNodes(node.children))
+    })
 
     // Construct new elements
-    val newTitle = ws.annotateSingle(r.title.label) match {
-      case Some(spots) => r.title.copy(oSpots = Some(spots))
-    }
+    val newTitle = r.title.copy(oSpots = Some(results(r.title)))
+
     val newOKeywords = r.oKeywords.map(keywords => {
-      val labels = keywords.map(_.label)
-      ws.annotateSeparately(labels) match {
-        case Some(l) => l.zip(keywords).map {
-          case(spots, keyword) => {
-            keyword.copy(oSpots = Some(spots))
-          }
-        }
-      }
+      keywords.map(keyword => {
+        keyword.copy(oSpots = Some(results(keyword)))
+      })
     })
+
     val newOCategories = r.oCategories.map(categories => {
-      val labels = categories.map(_.label)
-      ws.annotateSeparately(labels) match {
-        case Some(l) => l.zip(categories).map {
-          case (spots, category) => {
-            category.copy(oSpots = Some(spots))
-          }
-        }
-      }
+      categories.map(category => {
+        category.copy(oSpots = Some(results(category)))
+      })
     })
+
     val newODomains = r.oDomains.map(domains => {
-      val labels = domains.map(_.label)
-      ws.annotateSeparately(labels) match {
-        case Some(l) => l.zip(domains).map {
-          case (spots, domain) => {
-            domain.copy(oSpots = Some(spots))
-          }
-        }
-      }
+      domains.map(domain => {
+        domain.copy(oSpots = Some(results(domain)))
+      })
     })
+
     val newOSubdomains = r.oSubdomains.map(subdomains => {
-      val labels = subdomains.map(_.label)
-      ws.annotateSeparately(labels) match {
-        case Some(l) => l.zip(subdomains).map {
-          case (spots, subdomain) => {
-            subdomain.copy(oSpots = Some(spots))
-          }
-        }
-      }
+      subdomains.map(subdomain => {
+        subdomain.copy(oSpots = Some(results(subdomain)))
+      })
     })
 
-    val newOTocs = r.oTocs.map(tocs => tocs.map(toc => {
-      val newNodes = annotateNodes(toc.nodes)
-      toc.copy(nodes = newNodes)
-    }))
+    val newOTocs = r.oTocs.map(tocs => {
+      tocs.map(toc => {
+        toc.copy(nodes = annotateNodes(toc.nodes))
+      })
+    })
 
-    val newODescriptions = r.oDescriptions.map(descriptions =>
-      ws.annotateSeparately(descriptions.map(_.text)) match {
-        case Some(l) => l.zip(descriptions).map {
-          case (spots, description) => {
-            description.copy(oSpots = Some(spots))
-          }
-        }
-      }
-    )
+    val newODescriptions = r.oDescriptions.map(descriptions => {
+      descriptions.map(description => {
+        description.copy(oSpots = Some(results(description)))
+      })
+    })
 
     // Create the new resource
     r.copy(
