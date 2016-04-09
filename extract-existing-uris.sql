dd879813125d3ec03c6065f919171bd64dbbd73b
Frédérand Ouweric <pacm@users.noreply.github.com>
2016-04-09 10:09:26 +0200

    fix-candidate: escaped encoded utf-8 labels (#39)

--- extract-existing-uris.sql
+++ extract-existing-uris.sql
@@ -8,9 +8,7 @@
 # () It is used notably by the "user-search autocompletion" system
 
 # --------------------------------------------------------------------
-stop
-
-# todo: put CHARACTER SET 'utf8' everywhere!
+If launched, this script will crash because of this line :p
 
 # Create a table `existing-uris` with
 # a Uri (Varchar(255)) table utf-8-general_ci
@@ -51,14 +49,35 @@ CREATE TABLE `labels` (
   `Label` VARCHAR(255) CHARACTER SET 'utf8' NOT NULL,
   PRIMARY KEY (`Uri`, `Label`));
   
+# Note: Labels are a bit tricky to import because they contain
+# "escaped unicodes" (e.g. \u00A3) that need to be converted into
+# utf-8 and then imported to mysql with a special utf-8 option.
+# It is not dbpedia's fault, it is apparently the recommended
+# way to encode triplets in the "semantic web".
+# Note that the "uris" are encoded differently, but it is OK
+# to let them unchanged since it is only used as an "id" internally
+# or may be used to generate wikipedia URLs (it works as it).
+# i.e. We will not display "uris" to the user.
+#
+# Here is an interesting snippet of the labels from dbpedia
+# "%22Weird_Al%22_Yankovic_(album)","\"Weird Al\" Yankovic (album)"
+# "%60Abdu'l-Bah%C3%A1","`Abdu'l-Bah\u00E1"
+# "%C7%83X%C3%B3%C3%B5_language","\u01C3X\u00F3\u00F5 language"
+# "!X%C5%A9_mythology","!X\u0169 mythology"
+# "$on%C2%A5","$on\u00A5"
+# "$teve_Ba%C2%A3%C2%A3mer","$teve Ba\u00A3\u00A3mer"
+# "%229%E2%80%939%E2%80%939%22_plan","\"9\u20139\u20139\" plan"
+# "%22Christian_T%C4%83ma%C8%99%22","\"Christian T\u0103ma\u0219\""
+#
 # (1:manually) Download labels_en.nt from the dbpedia project
 # (2:bash) cp labels_en.nt labels_en.csv
 # (3:bash) sed -ri 's/^<http:\/\/dbpedia\.org\/resource\/(.+)?> <http:\/\/www\.w3\.org\/2000\/01\/rdf-schema#label> "(.+)?"@en \.$/\"\1\",\"\2\"/' labels_en.csv
-# (4.1:bash) if necessary: tail -n +2 labels_en.csv > labels.csv
-# (4.2:bash) if necessary: sed -i '$ d' labels.csv
-# (5:bash) mkfifo labels.fifo
-# (6:bash-blocking) pv labels.csv > labels.fifo
-# (7:bash) FILE='labels.fifo'; WORKDIR=`pwd`; mysql -u YOUR_USER -pYOUR_PASSWORD YOUR_DB_NAME --execute="LOAD DATA INFILE '$WORKDIR/$FILE' IGNORE INTO TABLE labels FIELDS TERMINATED BY ',' ENCLOSED BY '\"'; SHOW WARNINGS" > labels.warnings.log &
+# (4.1:bash) if necessary(discard first line): tail -n +2 labels_en.csv > labels.csv
+# (4.2:bash) if necessary(discard last line): sed -i '$ d' labels.csv
+# (5:bash) native2ascii -encoding UTF-8 -reverse labels.csv labels.utf8.csv
+# (6:bash) mkfifo labels.utf8.fifo
+# (7:bash-blocking) pv labels.utf8.csv > labels.utf8.fifo
+# (8:bash) FILE='labels.utf8.fifo'; WORKDIR=`pwd`; mysql -u YOUR_USER -pYOUR_PASSWORD YOUR_DB_NAME --execute="LOAD DATA INFILE '$WORKDIR/$FILE' IGNORE INTO TABLE labels CHARACTER SET UTF8 FIELDS TERMINATED BY ',' ENCLOSED BY '\"'; SHOW WARNINGS" > labels.utf8.warnings.log &
 
 # Create table `transitive-redirects`
 CREATE TABLE `transitive-redirects` (
@@ -71,12 +90,11 @@ CREATE TABLE `transitive-redirects` (
 # (1:manually) Download transitive-redirects_en.nt from the dbpedia project
 # (2:bash) cp transitive-redirects_en.nt transitive-redirects_en.csv
 # (3:bash) sed -ri 's/^<http:\/\/dbpedia\.org\/resource\/(.+)?> <http:\/\/dbpedia\.org\/ontology\/wikiPageRedirects> <http:\/\/dbpedia\.org\/resource\/(.+)?> \.$/\"\1\",\"\2\"/' transitive-redirects_en.csv
-# (4.1:bash) if necessary: tail -n +2 transitive-redirects_en.csv > transitive-redirects.csv
-# (4.2:bash) if necessary: sed -i '$ d' transitive-redirects.csv
+# (4.1:bash) if necessary(discard first line): tail -n +2 transitive-redirects_en.csv > transitive-redirects.csv
+# (4.2:bash) if necessary(discard last line): sed -i '$ d' transitive-redirects.csv
 # (5:bash) mkfifo transitive-redirects.fifo
 # (6:bash-blocking) pv transitive-redirects.csv > transitive-redirects.fifo
-# todo: log the warnings, skip msgs of the following cmd
-# (7:bash) FILE='transitive-redirects.fifo'; WORKDIR=`pwd`; mysql -u YOUR_USER -pYOUR_PASSWORD YOUR_DB_NAME --execute="LOAD DATA INFILE '$WORKDIR/$FILE' IGNORE INTO TABLE labels FIELDS TERMINATED BY ',' ENCLOSED BY '\"'; SHOW WARNINGS" > transitive-redirects.warnings.log &
+# (7:bash) FILE='transitive-redirects.fifo'; WORKDIR=`pwd`; mysql -u YOUR_USER -pYOUR_PASSWORD YOUR_DB_NAME --execute="LOAD DATA INFILE '$WORKDIR/$FILE' IGNORE INTO TABLE transitive-redirects FIELDS TERMINATED BY ',' ENCLOSED BY '\"'; SHOW WARNINGS" > transitive-redirects.warnings.log &
 
 # Create table `transitive-redirects2`
 CREATE TABLE `transitive-redirects2` (
@@ -125,16 +143,20 @@ INSERT IGNORE INTO `dictionary` (Uri, Label)
 ;
 
 # Create table `disambiguation`
-CREATE TABLE `disambiguation` (
+CREATE TABLE `disambiguations` (
   `A` VARCHAR(255) NOT NULL,
   `B` VARCHAR(255) CHARACTER SET 'utf8' NOT NULL,
   PRIMARY KEY (`A`, `B`))
 ;
-
-# todo: Check URI: !Qu%C3%A9_Clase_de_Amor! is correctly encoded
-# todo: Check URI: %60Abdu'l-Bah%C3%A1 is correctly encoded
-# todo: Check URI: %22Weird_Al%22_Yankovic_(album) is correctly encoded
-# todo: Check URI: %22Devious%22_Diesel -> "Devious" Diesel
+  
+# (1:manually) Download disambiguations_en.nt from the dbpedia project
+# (2:bash) cp disambiguations_en.nt disambiguations_en.csv
+# (3:bash) sed -ri 's/^<http:\/\/dbpedia\.org\/resource\/(.+)?> <http:\/\/dbpedia\.org\/ontology\/wikiPageDisambiguates> <http:\/\/dbpedia\.org\/resource\/(.+)?> \.$/\"\1\",\"\2\"/' disambiguations_en.csv
+# (4.1:bash) if necessary(discard first line): tail -n +2 disambiguations_en.csv > disambiguations.csv
+# (4.2:bash) if necessary(discard last line): sed -i '$ d' disambiguations.csv
+# (5:bash) mkfifo disambiguations.fifo
+# (6:bash-blocking) pv disambiguations.csv > disambiguations.fifo
+# (7:bash) FILE='disambiguations.fifo'; WORKDIR=`pwd`; mysql -u YOUR_USER -pYOUR_PASSWORD YOUR_DB_NAME --execute="LOAD DATA INFILE '$WORKDIR/$FILE' IGNORE INTO TABLE disambiguations FIELDS TERMINATED BY ',' ENCLOSED BY '\"'; SHOW WARNINGS" > disambiguations.warnings.log &
 
 
 
