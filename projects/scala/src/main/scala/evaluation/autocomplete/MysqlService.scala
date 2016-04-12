c1be1cd5dec2b75f6a06a9f6671b171f6edadada
Frédérand Ouweric <pacm@users.noreply.github.com>
2016-04-12 22:12:35 +0200

    avg-statistic (#39)

--- projects/scala/src/main/scala/evaluation/autocomplete/MysqlService.scala
+++ projects/scala/src/main/scala/evaluation/autocomplete/MysqlService.scala
@@ -1,5 +1,7 @@
 package evaluation.autocomplete
 
+import java.util.concurrent.atomic.AtomicInteger
+
 import slick.jdbc.JdbcBackend._
 import ws.autocomplete.Queries
 
@@ -20,10 +22,16 @@ object MysqlService extends App {
       gen(n - 1, pre + alphabet(i))
     }
   }
-  val words = List("hel", "wha", "why", "har", "spr", "cle", "com", "bio", "car", "fre")
+  val words = List("spring", "springfield", "simpsons", "coca", "Fanta")
+  val pre = List("abu", "spr", "cle", "com", "bio", "car", "fre")
   def gen2(): String = {
-    val i = math.floor(math.random * words.length).toInt
-    words(i) + gen(1)
+    if(math.random < 0.8) {
+      val i = math.floor(math.random * words.length).toInt
+      words(i)
+    } else {
+      val i = math.floor(math.random * pre.length).toInt
+      pre(i) + gen(1)
+    }
   }
 
   // Function to profile the amount of elapsed time
@@ -35,16 +43,28 @@ object MysqlService extends App {
   // Run the evaluation
   try {
     var latency = 500;
-    val ns = (1 to 1000).toList
+    val count = new AtomicInteger(0)
+    val sum = new AtomicInteger(0)
+    val ns = (1 to 10000).toList
     val futures = ns.map { n =>
       Thread.sleep(latency)
-      latency = math.max(20, latency - (latency / 10))
+      latency = math.max(10, latency - (latency / 10))
       val start = System.nanoTime()
       val text = gen2()
       val test1 = Queries.fourPlus(text, 10)
 
       db.run(test1.map(rs => {
-        println(s"....ok $text " + elapsedMs(start))
+        val elapsed = elapsedMs(start)
+        // Filter the outliers
+        if(elapsed > 200) {
+          println(s"....ok $text, avg= __, cur=" + elapsed)
+        } else {
+          val n = count.incrementAndGet()
+          val s = sum.addAndGet(elapsed)
+
+          val avg = (s / n)
+          println(s"....ok $text, avg= $avg, cur=" + elapsed)
+        }
       })).recover({
         case _ => {
           println(s"failed $text" + " " + elapsedMs(start))
