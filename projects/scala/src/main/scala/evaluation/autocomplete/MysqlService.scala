fb5dbf98425f5195c3a3f3fae5685f81e4257ead
Frédérand Ouweric <pacm@users.noreply.github.com>
2016-04-12 16:06:06 +0200

    sample evaluation code (#39)

---
+++ projects/scala/src/main/scala/evaluation/autocomplete/MysqlService.scala
@@ -0,0 +1,58 @@
+package evaluation.autocomplete
+
+import slick.jdbc.JdbcBackend._
+import ws.autocomplete.Queries
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
+
+object MysqlService extends App {
+
+  val db = Database.forConfig("wikichimp.autocomplete.slick")
+
+  // Functions to generate the text of each search
+  val alphabet = "abcdefghijklmnopqrstuvwxyz"
+  def gen(n: Int, pre: String = ""): String = n match {
+    case neg if neg <= 0 => pre
+    case _ => {
+      val i = math.floor(math.random * alphabet.length).toInt
+      gen(n - 1, pre + alphabet(i))
+    }
+  }
+  val words = List("hel", "wha", "why", "har", "spr", "cle", "com", "bio", "car", "fre")
+  def gen2(): String = {
+    val i = math.floor(math.random * words.length).toInt
+    words(i) + gen(1)
+  }
+
+  // Function to profile the amount of elapsed time
+  def elapsedMs(start: Long): Int = {
+    val stop = System.nanoTime()
+    ((stop - start) / 1000 / 1000).toInt
+  }
+
+  // Run the evaluation
+  try {
+    var latency = 500;
+    val ns = (1 to 1000).toList
+    val futures = ns.map { n =>
+      Thread.sleep(latency)
+      latency = math.max(20, latency - (latency / 10))
+      val start = System.nanoTime()
+      val text = gen2()
+      val test1 = Queries.fourPlus(text, 10)
+
+      db.run(test1.map(rs => {
+        println(s"....ok $text " + elapsedMs(start))
+      })).recover({
+        case _ => {
+          println(s"failed $text" + " " + elapsedMs(start))
+        }
+      })
+    }
+
+
+    Await.result(Future.sequence(futures), Duration.Inf)
+  } finally db.close
+}
