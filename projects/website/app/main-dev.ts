c5cbcaa459c62bb66b1280501e1b2a8c333bd948
Frédérand Ouweric <pacm@users.noreply.github.com>
2016-04-25 09:34:42 +0200

    prod-dev websites (#42)

--- projects/website/app/main-dev.ts
+++ projects/website/app/main-dev.ts
@@ -2,11 +2,9 @@ import {bootstrap} from "angular2/platform/browser";
 import {HTTP_PROVIDERS} from "angular2/http";
 import {ROUTER_PROVIDERS} from "angular2/router";
 import {AppComponent} from "./app.component";
-import {provide, enableProdMode} from "angular2/core";
-import {AppSettings} from "./config";
+import {provide} from "angular2/core";
+import {AppSettings} from "./config-dev";
 import {AngularFixService} from "./utils/angular-fix.service";
-
-enableProdMode();
 
 bootstrap(AppComponent, [
     HTTP_PROVIDERS,
