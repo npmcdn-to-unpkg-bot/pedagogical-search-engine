1ca225d9858819f0727536f58700e0bd11c112ce
Frédérand Ouweric <pacm@users.noreply.github.com>
2016-05-02 19:01:51 +0200

    create the searches table (#46)

--- projects/scripts/user-study/build-database.sql
+++ projects/scripts/user-study/build-database.sql
@@ -16,11 +16,10 @@ CREATE TABLE `clicks` (
     `EntryId` VARCHAR(36) CHARACTER SET 'utf8',
 	INDEX entryid_idx (`EntryId`(10)),
     
-	`Uris` TEXT CHARACTER SET 'utf8',
-	`Href` TEXT CHARACTER SET 'utf8',
-	`Source` TINYINT,
+	`SearchHash` INT,
 	`Rank` TINYINT,
 	`Quality` VARCHAR(32) CHARACTER SET 'utf8',
+    `Timestamp` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     
 	PRIMARY KEY (`AutoId`))
     CHARACTER SET 'utf8';
@@ -37,13 +36,35 @@ CREATE TABLE `classification` (
     # is not really a problem if there are not too many classification
     # entries per uris. e.g. We could compact/aggregate/sum
     # the table when it reaches a some size.
-    `Uris` TEXT CHARACTER SET 'utf8',
-	INDEX uris_idx (`Uris`(64)),
-    
+    `SearchHash` INT,
     `EntryId` VARCHAR(36) CHARACTER SET 'utf8',
-    `Classification` VARCHAR(32) CHARACTER SET 'utf8',
+	INDEX entry_idx (`SearchHash`, `EntryId`),
+    
+    `Classification` VARCHAR(16) CHARACTER SET 'utf8',
+    `Timestamp` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     
 	PRIMARY KEY (`AutoId`))
     CHARACTER SET 'utf8';
     
+
+CREATE TABLE `searches` (
+	# There will be a lot of inserts. Thus the "clustered-index"
+    # (aka the PK) in InnoDb should be consecutive.
+	`AutoId` INT NOT NULL AUTO_INCREMENT,
     
+    # But we want to group by uris to know how many times a search
+    # has been performed.
+    # Hence we create an index on this column.
+    # The records will not be consecutive on this column, but it
+    # is not really a problem if there are not too many classification
+    # entries per searches. e.g. We could compact/aggregate/sum
+    # the table when it reaches a some size.
+    `SearchHash` INT,
+	INDEX searchhash_idx (`SearchHash`),
+    
+    `Uris` TEXT CHARACTER SET 'utf8',
+    `Timestamp` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    
+
+	PRIMARY KEY(`AutoId`))
+    CHARACTER SET 'utf8';
