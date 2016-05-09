f82cb18772238bd2af20653a38c326fbc9fe2915
Frédérand Ouweric <pacm@users.noreply.github.com>
2016-05-09 13:26:18 +0200

    sort by availability, limit disambiguations (#54)

--- projects/scala/src/main/scala/ws/autocomplete/ranking/SizeFirst.scala
+++ projects/scala/src/main/scala/ws/autocomplete/ranking/SizeFirst.scala
@@ -16,9 +16,9 @@ object SizeFirst extends  Ranking {
       case (length, _) => length
     }.flatMap {
       case (l, rs) => rs.sortBy {
-        case Disambiguation(_, _, _, _) => Integer.MIN_VALUE // Disambiguations first
-        case Redirect(_, _, _, in, _) => -in // max |In| then
-        case Title(_, _, in, _) => -in
+        case Disambiguation(_, _, _, a) => (!a, Integer.MIN_VALUE) // Disambiguations first
+        case Redirect(_, _, _, in, a) => (!a, -in) // max |In| then
+        case Title(_, _, in, a) => (!a, -in)
       }
     }.foldLeft(List[Result]()) {
       // Filter out duplicate "uri"s
@@ -27,8 +27,17 @@ object SizeFirst extends  Ranking {
           case true => acc
           case false => acc ::: List(result)
         }
+    }.map {
+      // Sort disambiguation entries by popularity
+      // and limit their number
+      case e@Disambiguation(_, _, xs, _) =>
+        e.copy(bs = xs.sortBy {
+          case x => (!x.available, -x.in)
+        }.take(sc.maxRes))
+
+      case e => e
     }
   }
 
-  def projectSize(s: String, text: String): Int = math.min(s.length, text.length + 3)
+  def projectSize(s: String, text: String): Int = math.min(s.length, text.length + 2)
 }
