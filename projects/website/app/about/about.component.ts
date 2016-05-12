42e851ebf0c3b0a476964e16e73153527d891ffc
Frédérand Ouweric <pacm@users.noreply.github.com>
2016-05-12 14:44:45 +0200

    sketch about text (#48)

---
+++ projects/website/app/about/about.component.ts
@@ -0,0 +1,15 @@
+
+/*
+ Your searches and clicks is your data
+ and it is beautiful this way
+
+ but your data is also a great fuel to improve the
+ results through statistical studies if collected anonymously.
+
+ Here is our compromise: 
+ We forget about you as soon as you leave.
+ No cookies, no browser tracking.
+
+ We log the clicks, the searches but
+ We do not associate this data with you.
+ */
