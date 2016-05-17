846e5cd1a9278343cbe426f1350e4f0786b4ee6e
Frédérand Ouweric <pacm@users.noreply.github.com>
2016-05-17 22:50:18 +0200

    update sample config (#42)

--- projects/website/app/config.sample.ts
+++ projects/website/app/config.sample.ts
@@ -1,7 +1,8 @@
 
 export class AppSettings {
     public static settings = {
-        AUTOCOMPLETE_URL: "http://localhost:8181/autocomplete",
-        ENTRIES_URL: "http://localhost:8282/entries"
+        AUTOCOMPLETE_URL: "http://localhost:8180/autocomplete",
+        ENTRIES_URL: "http://localhost:8280/indices",
+        STUDY_URL: "http://localhost:8380"
     };
 }
