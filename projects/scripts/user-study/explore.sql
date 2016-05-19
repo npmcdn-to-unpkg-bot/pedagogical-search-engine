4c8f1f93d586b0634ab92263f3e909f4f0eda116
Frédérand Ouweric <pacm@users.noreply.github.com>
2016-05-19 23:28:54 +0200

    explore by date (#58)

--- projects/scripts/user-study/explore.sql
+++ projects/scripts/user-study/explore.sql
@@ -3,14 +3,18 @@ If launched, this script will crash because of this line :p
 ;
 
 SELECT * FROM `messages`
-WHERE Timestamp > STR_TO_DATE('18/05/2016 20:00:00', '%d/%m/%Y %H:%i:%s');
+WHERE Timestamp > STR_TO_DATE('18/05/2016 20:00:00', '%d/%m/%Y %H:%i:%s')
+ORDER BY Timestamp DESC;
 
 SELECT * FROM `searches`
-WHERE Timestamp > STR_TO_DATE('18/05/2016 20:00:00', '%d/%m/%Y %H:%i:%s');
+WHERE Timestamp > STR_TO_DATE('18/05/2016 20:00:00', '%d/%m/%Y %H:%i:%s')
+ORDER BY Timestamp DESC;
 
 SELECT * FROM `clicks`
-WHERE Timestamp > STR_TO_DATE('18/05/2016 20:00:00', '%d/%m/%Y %H:%i:%s');
+WHERE Timestamp > STR_TO_DATE('18/05/2016 20:00:00', '%d/%m/%Y %H:%i:%s')
+ORDER BY Timestamp DESC;
 
 SELECT * FROM `classifications`
-WHERE Timestamp > STR_TO_DATE('18/05/2016 20:00:00', '%d/%m/%Y %H:%i:%s');
+WHERE Timestamp > STR_TO_DATE('18/05/2016 20:00:00', '%d/%m/%Y %H:%i:%s')
+ORDER BY Timestamp DESC;
 
