dc97dd72479a2dbc47cea4a026221c46673842f3
Frédérand Ouweric <pacm@users.noreply.github.com>
2016-05-30 22:58:17 +0200

    fix missing parameter and case (#00b)

--- projects/scala/src/main/scala/agents/IndexWithGraphs.scala
+++ projects/scala/src/main/scala/agents/IndexWithGraphs.scala
@@ -17,7 +17,9 @@ object IndexWithGraphs extends App with Formatters {
   val settings = new Settings()
 
   // Create the file explorer
-  val explorer = new FileExplorer("index-with-graphs", forceProcess = true)
+  val explorer = new FileExplorer("index-with-graphs",
+    nbTasks = settings.Indices.Indexation.nbTasks,
+    forceProcess = true)
 
   // Define how the resources are processed
   def process(file: File, ec: ExecutionContext): Future[Any] = {
@@ -28,7 +30,9 @@ object IndexWithGraphs extends App with Formatters {
     // Index
     val indexer = new Graph(ec)
     indexer.index(r).map {
-      case Some(newR) => Json.write(newR, Some(file.getAbsolutePath))
+      case Some(newR) =>
+        Json.write(newR, Some(file.getAbsolutePath))
+      case None => // The resource was rejected, we do nothing
     }(ec)
   }
 
